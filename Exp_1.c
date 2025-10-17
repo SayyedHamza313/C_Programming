e9a7afee6d92da9adee4654982f3e4106b309752
SayyedHamza313 <hamzasayyed0313@gmail.com>
2025-10-17 14:20:28 +0530

    Create Exp_1.c

---
+++ Exp_1.c
@@ -0,0 +1,19 @@
+/*
+NAME:Sayyed Mohammad Hamza Asif
+UIN:251M002
+CLASS:F.E MECHANICAL
+DIV:F 
+*/
+#include <stdio.h>
+
+int main()
+{    
+    
+char Name [50];
+printf ("Your Name: Sayyed Mohammad Hamzza Asif\n");
+printf ("Your Age:19\n");
+printf ("Class:F.E Mechanical\n");
+printf ("DIV:F\n");
+printf ("UIN:251M002\n");
+return 0;
+}
