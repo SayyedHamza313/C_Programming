c542e7f85ee78be119725a126d60ac028cf0f7f4
SayyedHamza313 <hamzasayyed0313@gmail.com>
2025-11-12 11:51:50 +0530

    Create Exp 2a .C

---
+++ Exp 2a .C
@@ -0,0 +1,22 @@
+/*Name: Sayyed Mohammad Hamza Asif 
+UIN:251M002
+Class: FE Mechanical 
+Div:FE
+*/
+#include <stdio.h>
+
+int main() {
+    int num;
+    
+    // Input from user
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    
+    // Check even or odd
+    if (num % 2 == 0)
+        printf("%d is even.\n", num);
+    else
+        printf("%d is odd.\n", num);
+    
+    return 0;
+}
