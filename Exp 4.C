6587a369e9667214a791bd3989940cee80db465a
SayyedHamza313 <hamzasayyed0313@gmail.com>
2025-11-22 22:51:22 +0530

    Exp 4.C

---
+++ Exp 4.C
@@ -0,0 +1,31 @@
+Name: Sayyed Mohammad Hamza Asif 
+UIN: 251M002
+Roll No:2
+Class:FE Mechanical 
+Div:F
+*/
+#include <stdio.h>
+
+int isPrime(int num) {
+    if (num <= 1) return 0;
+    for (int i = 2; i <= num / 2; i++) {
+        if (num % i == 0)
+            return 0;
+    }
+    return 1;
+}
+
+int main() {
+    int lower, upper;
+    printf("Enter first number: ");
+    scanf("%d", &lower);
+    printf("Enter second number: ");
+    scanf("%d", &upper);
+
+    printf("Prime numbers between %d and %d are:\n", lower, upper);
+    for (int i = lower; i <= upper; i++) {
+        if (isPrime(i))
+            printf("%d ", i);
+    }
+    return 0;
+}
