e8d6682c1287ac3136ec60feb76ed92b833dc14b
SayyedHamza313 <hamzasayyed0313@gmail.com>
2025-11-22 22:58:49 +0530

    Exp 8b.C

---
+++ Exp 8b.C
@@ -0,0 +1,31 @@
+/*Name: Sayyed Mohammad Hamza Asif 
+UIN: 251M002
+Roll No:2
+Class:FE Mechanical 
+Div:F
+*/
+#include <stdio.h>
+int main() {
+    char str[100];
+    int i, j, flag = 0;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    for (i = 0; str[i] != '\0'; i++);
+
+  
+    for (j = 0; j < i / 2; j++) {
+        if (str[j] != str[i - j - 1]) {
+            flag = 1;
+            break;
+        }
+    }
+
+    if (flag == 0)
+        printf("The string is a palindrome.");
+    else
+        printf("The string is NOT a palindrome.");
+
+    return 0;
+}
